b2b2dc36cb038b3fabec2ce9a014dd4fce5c6938
mhinkle98 <44630605+mhinkle98@users.noreply.github.com>
2019-04-28 15:03:28 -0500

    P3

--- Exam3/Exam3.m
+++ Exam3/Exam3.m
@@ -6,3 +6,21 @@ disp(recursiveFac(7)/(recursiveFac(3) * recursiveFac(4)));
 
 %%
 % P2
+clear; clc;
+x = -2*pi: 1/5: 2*pi;
+y = sin(x);
+N = length(x);
+for i = 1:N
+    plot(x(i),y,'r*')
+    axis([min(x)-1 max(x)+1 min(y)-1 min(y)+1])
+    M(i) = getframe;
+    movie(M)
+end
+M(i) = getframe;
+    
+movie(M)
+
+%%
+% P3
+
+circuitFunction();
---
+++ Exam3/circuitFunction.m
@@ -0,0 +1,17 @@
+function circuitFunction()
+%CIRCUITFUNCTION Plots our circuit results through 1,000 simulations
+
+hold on
+for i = 1:1001
+    r = -5 + (5+5)*rand(1);
+    Resistance = 1000 + 0.01*r*1000;
+    r2 = -10 + (10 + 10)*rand(1);
+    C = 0.000001 + 0.01*r2*0.000001;
+    denom = 2 * pi * Resistance * C;
+    frequency = 1 / denom;
+    plot(Resistance, frequency, 'o');
+end
+hold off
+
+end
+
