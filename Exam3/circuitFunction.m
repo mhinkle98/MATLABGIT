7f40a919faafd39a2ffe07b98c4e86fb2a3ccbf7
mhinkle98 <44630605+mhinkle98@users.noreply.github.com>
2019-04-28 15:11:19 -0500

    Update circuitFunction.m

--- Exam3/circuitFunction.m
+++ Exam3/circuitFunction.m
@@ -6,7 +6,7 @@ title("Cutoff Frequencies over 1000 Simulations");
 xlabel("Resistance");
 ylabel("Frequency");
 hold on
-
+values = zeros(1, 1000);
 for i = 1:1001
     r = -5 + (5+5)*rand(1);
     Resistance = 1000 + 0.01*r*1000;
@@ -14,9 +14,11 @@ for i = 1:1001
     C = 0.000001 + 0.01*r2*0.000001;
     denom = 2 * pi * Resistance * C;
     frequency = 1 / denom;
+    values(i) = frequency;
     plot(Resistance, frequency, 'o');
 end
 hold off
-
+figure();
+histogram(values);
 end
 
