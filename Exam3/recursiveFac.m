f9c3010e657be6ace40e1c25f898a1d762d546de
mhinkle98 <44630605+mhinkle98@users.noreply.github.com>
2019-04-29 16:27:38 -0500

    Finished with GitIgnore

--- Exam3/recursiveFac.m
+++ Exam3/recursiveFac.m
@@ -1,5 +1,5 @@
 function answer = recursiveFac(number)
-%% Write a recursive function file to compute  x =7!/(3!4!); where n! = n(n?1)(n?2)..(2)(1).
+%% Write a recursive function file to compute  x =7!/(3!4!); where n! = n(n-1)(n-2)..(2)(1).
 if number <= 1
     answer = 1;
 else
