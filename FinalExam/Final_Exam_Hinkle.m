9e397089fce9c8e9b6b04a603c8e971116fd9cec
mhinkle98 <44630605+mhinkle98@users.noreply.github.com>
2019-05-11 09:12:02 -0500

    Create Final_Exam_Hinkle.m
    Initial

---
+++ FinalExam/Final_Exam_Hinkle.m
@@ -0,0 +1,28 @@
+%% Max Hinkle
+% BME 2020 Computing Concepts & Applications
+%%%%%%%%%%%%%%%% FINAL EXAM %%%%%%%%%%%%%%%%
+
+%%
+% P1
+clear; clc;
+% Part A
+
+a = 6 * (10/13) + 18/(5*7) + 5 * 9^2
+
+% Part B
+
+b = 6 * 35^(1/4) + 14^0.35
+
+
+%%
+% P2
+clear; clc;
+x = -5 + 9j;
+y = 6 - 2j;
+% Part A
+
+a = x + y
+
+% Part B
+
+b = (x*y) / (3*x - 4*y)^3
